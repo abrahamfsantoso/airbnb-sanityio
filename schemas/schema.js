6816601a152c52ac63378ee971be623a9dec9d81
Abraham Fajar Santoso <abrahamfsantoso@gmail.com>
2021-08-16 16:49:18 +0700

    added host, person and traveller schema

--- schemas/schema.js
+++ schemas/schema.js
@@ -8,6 +8,9 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 import property from './property'
 import propertyImage from './propertyImage'
 import review from './review'
+import traveller from './traveller'
+import host from './host'
+import person from './person'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -19,6 +22,9 @@ export default createSchema({
     /* Your types here! */
     property,
     propertyImage,
-    review
+    review, 
+    traveller,
+    host,
+    person
   ]),
 })
